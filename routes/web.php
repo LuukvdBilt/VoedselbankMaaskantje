4529c29da42ecfeeca3428a4373e7f803f953196
wesley <340308@student.mboutrecht.nl>
2026-04-09 10:08:17 +0200

    web link en redirect

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,16 @@
 <?php
 
+use App\Http\Controllers\InventoryController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome')->name('home');
 
+Route::get('/magazijn', [InventoryController::class, 'index']);
+
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::view('dashboard', 'dashboard')->name('dashboard');
 });
+
+
 
 require __DIR__.'/settings.php';
