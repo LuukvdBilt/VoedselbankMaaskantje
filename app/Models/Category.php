7e8c5848ac1d8f6a239062e9ba836c9678f44b53
wesley <340308@student.mboutrecht.nl>
2026-04-09 12:07:14 +0200

    category factory bug fix

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -1,16 +1,17 @@
 <?php
 
 namespace App\Models;
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Category
+class Category extends Model
 {
+    use HasFactory;
+
     protected $table = 'categories';
     protected $primaryKey = 'id';
-    protected $allowedFields = ['name', 'description'];
 
-    public function __construct()
-    {
-    }
+    // Mass assignment
+    protected $fillable = ['name', 'description'];
 }
