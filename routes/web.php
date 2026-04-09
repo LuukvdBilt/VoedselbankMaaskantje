1adafd1490ac40dd94539d86131eb92e57479dd2
wesley <340308@student.mboutrecht.nl>
2026-04-09 11:05:59 +0200

    checkrole bijna klaar

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@ Route::get('/magazijn', [InventoryController::class, 'index']);
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::view('dashboard', 'dashboard')->name('dashboard');
-    Route::resource('inventory', InventoryController::class);
+    Route::resource('inventory', InventoryController::class)->middleware('role:admin,manager');
 });
 
 //supplier routes
